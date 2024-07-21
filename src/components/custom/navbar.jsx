98f3ee1808819ca7c059408c44f4d9700b7f8807
Omar Mohamed <99252081+OmarMohammedd@users.noreply.github.com>
2024-07-21 16:14:53 +0300

    update

--- src/components/custom/navbar.jsx
+++ src/components/custom/navbar.jsx
@@ -377,8 +377,15 @@ const Navbar = () => {
                   <div className="flex flex-col gap-6">
 
 <div className="grid grid-cols-2 gap-3" dir={language ==='en' ? 'ltr' : 'rtl'}>
-      <div className="col-span-2 mb-1 text-sm/none font-medium md:mb-0">
+      <div className="col-span-2 mb-1 text-sm/none font-medium md:mb-0 flex items-center justify-between">
         {language ==='en' ? 'Language' : 'لغة'}
+        
+    <svg
+    onClick={handleButtonClick}
+     className="closee" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
+    <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+  </svg>
+
       </div>
       <button
         onClick={() => handleChangeLanguage('ar')}
